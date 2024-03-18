23b460689e9c3a290e4ea560c9fc8b5ec62604c6
Nichole Williams <nicci@nicciwill.com>
2024-03-17 20:18:00 -0400

    first nextjs project chapters 1-5

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,10 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    // fontFamily: {
+    //   primary: ['Inter', 'sans-serif'],
+    //   secondary: ['Lusitana', 'serif'],
+    // },
     extend: {
       gridTemplateColumns: {
         '13': 'repeat(13, minmax(0, 1fr))',
